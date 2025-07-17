f165358bf8566c4b5fb3573c7eebdf6648d5ec9c
thebruhboozler <sababugs112@gmail.com>
2025-07-17 10:46:25 +0400

    added new voting methods

--- scripts/highest_average.lua
+++ scripts/highest_average.lua
@@ -2,6 +2,55 @@
 ---Ballot_Type: Point
 ---Description: picks the candidate with the highest average score 
 
+local candidateRatings = {}
 
----not implmeneted yet
-return 0
+for _, candidate in ipairs(candidates) do
+	table.insert(candidateRatings,{candidate = candidate , ratings = {}})
+end
+
+local function addRating(candidate, rating)
+	for _, entry in ipairs(candidateRatings) do
+		if entry.candidate == candidate then
+			table.insert(entry.ratings, rating)
+		end
+	end
+end
+
+for _, vote in ipairs(votes)do
+	for candidate, rating in pairs(vote) do
+		addRating(candidate,rating)
+	end
+end
+
+local function getAverage(candidateRating)
+	local sum =0
+	for _, rating in ipairs(candidateRating.rating) do
+		if rating ~= -1 then
+			sum = sum + rating
+		end
+	end
+	return sum/#candidateRating.rating
+end
+
+local winningCandidate =""
+local highestAverage = 0
+
+for _, candidate in ipairs(candidateRatings) do
+	local avg = getAverage(candidate)
+	if avg > highestAverage then
+		highestAverage = avg
+		winningCandidate = candidate.candidate
+	end
+end
+
+
+local function keyOf(tbl, value)
+    for k, v in pairs(tbl) do
+        if v == value then
+            return k
+        end
+    end
+    return nil
+end
+
+return keyOf(winningCandidate,candidates)
---
+++ scripts/instant_runoff.lua
@@ -0,0 +1,58 @@
+---Title: Instant Runoff
+---Ballot_Type: Ranked
+---Description: each voter ranks the candidates from most preferred to least preferred. in the first round each candidate gets the number of votes according to how many times they were ranked most prefered. if the winning candidate doesn't achieve a majority vicotry, all the other most preferred candidates are removed from their ballots and the votes go to the second most preferred candidate. this process repeats until a majority winner is found
+
+
+local winningCandidate=""
+local maxVotes = 0
+local threshold = #votes*0.5
+
+
+local electionTracker ={}
+
+for _,candidate in pairs(candidates) do
+	table.insert(electionTracker,{candidate = candidate , numOfVotes = 0})
+end
+
+local function addVote(candidate)
+	for _,entry in pairs(electionTracker) do
+		if entry.candidate == candidate then
+			entry.numOfVotes = entry.numOfVotes + 1
+		end
+	end
+end
+
+local function keyOf(tbl, value)
+    for k, v in pairs(tbl) do
+        if v == value then
+            return k
+        end
+    end
+    return nil
+end
+
+
+while true
+do
+	maxVotes = 0
+	winningCandidate = ""
+	for _, vote in ipairs(votes) do
+		addVote(vote[1])
+	end
+
+	for _, entry in ipairs(electionTracker) do
+		if entry.numOfVotes > maxVotes then
+			maxVotes = entry.numOfVotes
+			winningCandidate = entry.candidate
+		end
+	end
+	if maxVotes >= threshold then
+		return keyOf(candidates, winningCandidate)
+	else
+		for _, vote in ipairs(votes) do
+			if vote[1] ~= winningCandidate then
+				table.remove(vote,1)
+			end
+		end
+	end
+end
